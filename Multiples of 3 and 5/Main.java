8d646552fb32afcae148759f471494dd39051836
Tuyền Nguyễn <tuyendev@gmail.com>
2017-08-30 15:13:19 +0700

    Create Main.java

---
+++ Multiples of 3 and 5/Main.java
@@ -0,0 +1,13 @@
+package com.tuyendev.test;
+
+public class Main {
+    public static void main(String[] args) {
+        long sum = 0;
+
+        for (int i = 0; i < 1e3; i++) {
+            if (i % 3 == 0 || i % 5 == 0) sum += i;
+        }
+
+        System.out.println(sum);
+    }
+}
