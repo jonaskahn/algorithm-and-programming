2fcd5e9707cac42dd1f6bba827483cbb751d5e7f
Tuyền Nguyễn <tuyendev@gmail.com>
2017-08-31 14:36:59 +0700

    Update Main.java

--- Even Fibonacci numbers/Main.java
+++ Even Fibonacci numbers/Main.java
@@ -1,1 +1,23 @@
 
+package com.tuyendev.test;
+
+public class Main {
+    public static void main(String[] args) {
+        int start1 = 1;
+        int start2 = 2;
+        long sum = start2;
+        int count = 0;
+        int nextnumber = 0;
+        while(nextnumber < 4_000_000){
+            nextnumber = start1+start2;
+            start1 = start2;
+            start2 = nextnumber;
+            count++;
+            if(count == 3){
+                count = 0;
+                sum+=nextnumber;
+            }
+        }
+        System.out.println(sum);
+    }
+}
